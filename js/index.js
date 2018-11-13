4b2898841e23af4eeb347114f4a8aaaac537ac1e
jeffberlin <jeff@jbrally.com>
2018-11-13 11:59:29 -0500

    Adding fade in/out for scroll button

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+$(function() {
+    var scrollUp = $("#topcontrol");
+    var bodyHeight = $("body").height();
+    $(window).scroll(function() {
+        if (bodyHeight <= $(window).height() + $(window).scrollTop() + 100) {
+            scrollUp.fadeIn();
+        } else {
+            scrollUp.hide();
+        }
+    });
+});
+
+// JS for scrolling back to top of the page
 $("#topcontrol").click(function() {
 	$('html, body').animate({
 		scrollTop: $("#primary").offset().top
