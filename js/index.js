ac51b8ca09aeb06e5613a14b645558baf5a64aab
jeffberlin <jeff@jbrally.com>
2018-11-14 16:01:50 -0500

    Building sewingProducts.html dynamically

--- js/index.js
+++ js/index.js
@@ -14,3 +14,52 @@ $("#topcontrol").click(function() {
 		scrollTop: $("#primary").offset().top
 	}, 'slow')
 })
+
+// Array for Sewing Products
+var sewingProducts = [
+	{
+		"company": "Threadsnscissors",
+		"url": "http://www.threadsnscissors.com/"
+	},
+	{
+		"company": "Bargain Embroidery Store",
+		"url": "http://www.bargainembstore.com/"
+	},
+	{
+		"company": "Inspiration Mutz Embroidery",
+		"url": "http://www.inspirationmutz.com/"
+	},
+	{
+		"company": "FromTheneedleOfAnne",
+		"url": "http://www.fromtheneedleofanne.com/"
+	},
+	{
+		"company": "Elsa's Designs",
+		"url": "http://www.elsasdesigns.com/"
+	},
+	{
+		"company": "Chantell's Creations",
+		"url": "http://www.chantells-creations.com/"
+	},
+	{
+		"company": "SoftSew",
+		"url": "http://www.softsew.com/"
+	},
+	{
+		"company": "Designs4Africa",
+		"url": "http://www.designs4africa.com/"
+	},
+	{
+		"company": "DesignsbyPetro",
+		"url": "http://www.designsbypetro.com/"
+	},
+	{
+		"company": "Diana's Designs",
+		"url": "http://www.dianasdezigns.com/"
+	},
+	{
+		"company": "Ellen Jaye Designs",
+		"url": "http://www.ellenjayedesigns.com/"
+	}
+]
+console.log(sewingProducts);
