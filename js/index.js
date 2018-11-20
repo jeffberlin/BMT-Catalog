c568fe911ca7a60c21ae1a430ac88e19bc9067f2
jeffberlin <jeff@jbrally.com>
2018-11-20 12:45:43 -0500

    Converting to AngularJS. Fixing productList.html

--- js/index.js
+++ js/index.js
@@ -16,18 +16,42 @@ $("#topcontrol").click(function() {
 })
 
 // Products search filter
-function searchFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("productInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
+// function searchFunction() {
+//     var input, filter, ul, li, a, i;
+//     input = document.getElementById("productInput");
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("myUL");
+//     li = ul.getElementsByTagName("li");
+//     for (i = 0; i < li.length; i++) {
+//         a = li[i].getElementsByTagName("a")[0];
+//         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+//             li[i].style.display = "";
+//         } else {
+//             li[i].style.display = "none";
+//         }
+//     }
+// }
+
+var app = angular.module('catalog', []);
+app.controller('catalogCtrl', function($scope, $document) {
+
+// product array
+	$scope.products = [
+		{
+			company: '3D Flash Animator',
+			url: 'http://www.3dfa.com/',
+			grouping: '0-9'
+		},
+		{
+			company: 'A+ Screensaver',
+			url: 'http://www.softdd.com/screens/index.htm',
+			grouping: 'A'
+		},
+		{
+			company: 'Batch Rename .EXE',
+			url: 'http://www.stintercorp.com/purchase/br/',
+			grouping: 'B'
+		}
+	]
+
+});
